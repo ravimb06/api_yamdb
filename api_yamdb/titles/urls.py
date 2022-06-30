279062cad2c3792e38419cf8c81fdb742821a9d3
Ali Bogatyrev <marksheyder06@gmail.com>
2022-06-30 18:30:44 +0300

    added urls

--- api_yamdb/titles/urls.py
+++ api_yamdb/titles/urls.py
@@ -6,7 +6,7 @@ from . import views
 router = DefaultRouter()
 router.register(r'categories', views.CategoryViewSet, basename='categories')
 router.register(r'genres', views.GenreViewSet, basename='genres')
-router.register(r'titles', views.TitleViewSet, basename='titles')
+router.register(r'titles', views.TitlesViewSet, basename='titles')
 
 urlpatterns = [
     path('v1/', include(router.urls)),
