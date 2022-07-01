5f28fc7a7b9872df5b74270a9fc3c50e67d82a64
Ali Bogatyrev <marksheyder06@gmail.com>
2022-07-01 22:44:31 +0300

    final version2

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -12,7 +12,6 @@ from api.serializers import (CategorySerializer, CommentSerializer,
 from django.core.mail import send_mail
 from django.db import IntegrityError
 from django.db.models import Avg
-
 from django.shortcuts import get_object_or_404
 
 from django_filters.rest_framework import DjangoFilterBackend
